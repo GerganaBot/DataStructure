c8cf6a3bdbd2dd3525830ddf8ce7c11a5272bbcd
Gergana Boteva <guergana.boteva1@gmail.com>
2023-03-04 23:08:25 +0000

    Concatenate strings function

---
+++ ConcatenateFunc.py
@@ -0,0 +1,18 @@
+﻿# Write a concatenate() function that receives some strings as arguments and some named arguments (the key will be a
+# string, and the value will be another string).
+# First, you should concatenate all arguments successively. Next, take each key successively, and if it is present in
+# the resulted string, change all matching parts with the key's value. In the end, return the final string.
+
+def concatenate(*args, **kwargs):
+    text = ''.join(args)
+
+    for key, value in kwargs.items():
+        text = text.replace(key, value)
+
+    return text
+
+
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
+
+
+
