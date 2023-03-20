ea33e33c446fdfb9c800261eb53c50db359e7db4
Gergana Boteva <guergana.boteva1@gmail.com>
2023-03-20 23:02:32 +0000

    Dictionary/tuple problem Count Symbols

---
+++ TupleDictCountSymbols.py
@@ -0,0 +1,14 @@
+﻿# Write a program that reads a text from the console and counts the occurrences of each character in it. Print the results
+# in alphabetical (lexicographical) order.
+
+text = input()
+
+symbols = {}
+for ch in text:
+    if ch in symbols:
+        symbols[ch] += 1
+    else:
+        symbols[ch] = 1
+
+for key, value in sorted(symbols.items()):
+    print(f'{key}: {value} time/s')
